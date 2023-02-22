972d99769577b876cf31f614b2944e568ba25734
Chasel <arko9988b@gmail.com>
2023-02-23 03:28:32 +0800

    working on get 10 issues from github

--- app/AfterLogin/page.tsx
+++ app/AfterLogin/page.tsx
@@ -3,98 +3,15 @@ import { useSearchParams } from 'next/navigation';
 import { useState, useEffect} from 'react';
 import useSWR from 'swr';
 
-class data {
-    title! : string
-}
-
-function GetIssues({token}:{token: string})
-{    
-    const [count, setCount] = useState<any>(null);
-    let owner = "LeeChasel";
-    let repo = "dcard_intern_homework"; 
-
-    // useEffect(() => {
-    //     const fetchdata = async () => {
-    //     const res = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues?state=all&per_page=2`, {
-    //     headers: {
-    //         "Accept" : "application/vnd.github+json",
-    //         "Authorization" : `Bearer ${token}`,
-    //     }}).then(res => res.json());
-    //     setCount(res);
-    // };
-    //     fetchdata();
-    // }, []);
-
-    // useEffect(() => {
-    //     fetch(`https://api.github.com/repos/${owner}/${repo}/issues?state=all&per_page=2`, {
-    //         headers: {
-    //                     "Accept" : "application/vnd.github+json",
-    //                     "Authorization" : `Bearer ${token}`,
-    //     }}).then((res) => res.json()).then((data) => setCount(data)).catch((err) => console.log(err));
-    // }, []);
-
-
-
-
-
-    // getdata().then(res => res.json()).then(r => setCount(r));
-    // getdata();
-    // console.log("qwert")
-    // return count;
-    // console.log(count)
-    // return (
-        
-        
-    //     <div>
-    //         {count ? (
-    //             <div>
-    //                 {JSON.stringify(count)}
-    //             </div>
-    //         ) : (
-    //             <h2>Loading</h2>
-    //         )}
-    //     </div>
-    // )
-
-    async function getdata() {
-        const res = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues?state=all&per_page=2`, {
-        headers: {
-            "Accept" : "application/vnd.github+json",
-            "Authorization" : `Bearer ${token}`,
-        }});        
-        // return res;
-    }
-    
-    // console.log(res)
-    // const ss = await res.json();
-    // console.log(ss);
-    
-}
-
-
-async function Test({token}: {token: string})
+class FormData
 {
-    const [count, setCount] = useState(0);
-    let owner = "LeeChasel";
-    let repo = "dcard_intern_homework";
-
-    // const fetcher = (ur: RequestInfo | URL) => fetch(ur, {
-    //     headers: {
-    //         "Accept" : "application/vnd.github+json",
-    //         "Authorization" : `Bearer ${token}`,
-    //     }
-    // }).then(r => r.json()).then(r => console.log(r[0]));
-    // const {data, error, isLoading} = useSWR(`https://api.github.com/repos/${owner}/${repo}/issues?state=all`, fetcher);
-    // if (error) return <div>failed to load</div>
-    // if (isLoading) return <div>loading...plz wait</div>
-    // return (
-    //     <>{typeof data}
-        
-    //         {/* <div>{data.title}</div> */}
-    //     </>
-    // )
-
+    url!: string;
+    html_url!: string;
+    title!: string;
+    body!: string; 
+    number!: string;   
 }
+
 function useUser(token: string)
 {
     let owner = "LeeChasel";
@@ -105,9 +22,8 @@ function useUser(token: string)
             "Authorization" : `Bearer ${token}`,
         }
     }).then(r => r.json());
-    const {data, error, isLoading} = useSWR(`https://api.github.com/repos/${owner}/${repo}/issues?state=all`, fetcher);
-
-
+    
+    const {data, error, isLoading} = useSWR(`https://api.github.com/repos/${owner}/${repo}/issues?state=all&per_page=10&page=2`, fetcher);
     return {
         user: data,
         isLoading,
@@ -119,31 +35,34 @@ function GetData({token}: {token: string})
 {
     const {user, isLoading, isError} = useUser(token);
     if (isLoading) return <div>Loading...</div>
-    if (isError) return <div>Error happened !!</div>
-    let obj = JSON.parse(user)
-
-    return (        
-        <div>{obj}</div>
-    )
-
-
-
+    if (isError) return <div>Error happened !!</div>    
+    var reverseUser = user.map((item: FormData) => item).reverse();
+    
+    return (
+        <div className="relative">
+            <div className='flex flex-col bg-gray-400 absolute left-1/2 top-1/2'>
+            {reverseUser.map((data: FormData) => {
+                return (                   
+                    <div key={data.number} className="flex flex-row">
+                        <h3 className='bg-red-200'>{data.title}</h3>
+                        <p className='bg-blue-200'>{data.body}</p>
+                    </div>
+                )
+            })          
+            }
+            </div>
+        </div>
+   )
 }
 
 export default function afterLogin()
 {
     const searchParams = useSearchParams();
     const token: string = searchParams.get("access_token")!;
-    const [count, setCount] = useState<any>(null);
-    // console.log("ss")
-    // let ss = getIssues(token);
+    // const [count, setCount] = useState<any>(null);
     return (
         <main>
-            {/* <GetIssues token = {token}/> */}
             <GetData token = {token}/>
-            
-            {/* <button onClick={() => getIssues(token)}>Click to get issues on console</button> */}
-            {/* <Test token={token}/> */}
         </main>
     )
 }
