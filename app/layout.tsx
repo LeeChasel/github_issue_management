b84375f761a3837e160789ab12037c8362cffe62
Chasel <arko9988b@gmail.com>
2023-03-28 02:10:52 +0800

    attempt to add auth.js and rebuild project architecture

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,23 @@
 import './globals.css'
+// import { SessionProvider } from "next-auth/react"
+import AuthContext from './AA';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
 }) {
+
   return (
-    <html lang="en" className='h-full'>
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body className='h-full'>{children}</body>
-    </html>
+    // <AuthContext>
+      <html lang="en" className='h-full'>
+        {/*
+          <head /> will contain the components returned by the nearest parent
+          head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+        */}
+        <head />
+        <body className='h-full'>{children}</body>
+      </html>
+      // </AuthContext>
   )
 }
